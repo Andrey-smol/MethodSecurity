e9619852aec9f8f316f43533168a29f6ce615698
Andrey-smol <andreyksmol@yandex.ru>
2025-08-04 17:45:17 +0300

    spring-boot-starter-data-mongodb connected

--- src/main/java/ru/netology/nosql_mongodb/controller/UserController.java
+++ src/main/java/ru/netology/nosql_mongodb/controller/UserController.java
@@ -20,13 +20,11 @@ public class UserController {
 
     @Autowired
     public UserController(UserService service) {
-
         this.service = service;
     }
 
     @GetMapping
     public List<User> getUsers() {
-
         return service.getUsers();
     }
 
@@ -34,14 +32,12 @@ public class UserController {
     @Secured({"ROLE_READ"})
     @PostAuthorize("returnObject.name == authentication.principal.username")
     public User getUserById(@PathVariable String id) {
-
         return service.getUserById(id);
     }
 
     @GetMapping("/name/{name}")
     @PreAuthorize("(#name == authentication.principal.username)")
     public List<User> getUsersByName(@PathVariable String name) {
-
         return service.getUsersByName(name);
     }
 
@@ -49,28 +45,24 @@ public class UserController {
     @GetMapping("/age/{age}")
     @Secured({"ROLE_READ"})
     public List<User> getUsersByAge(@PathVariable int age) {
-
         return service.getUsersByAge(age);
     }
 
     @PostMapping("/add_user")
     @RolesAllowed({"ROLE_WRITE"})
     public User addUser(@RequestBody @Valid User user) {
-
         return service.addUser(user);
     }
 
     @PutMapping("/update_user")
     @Secured({"ROLE_UPDATE"})
     public User updateUser(@RequestBody @Valid User user) {
-
         return service.updateUser(user);
     }
 
     @GetMapping("/del_user/{id}")
     @PreAuthorize("(hasAnyRole({'ROLE_WRITE', 'ROLE_DELETE'}))")
     public void deleteUser(@PathVariable String id) {
-
         service.deleteUser(id);
     }
 }
